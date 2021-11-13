19ebaaf3876cdf712ae158fe3bef5bd2419e1968
kayrab <50419086+kayrab@users.noreply.github.com>
2021-11-13 11:19:15 +0200

    Add files via upload

--- geom.hpp
+++ geom.hpp
@@ -40,7 +40,7 @@ struct Line {
     }
 
     bool parallel(const Line& other) const {
-        return eq(this->A/other.A, this->B/other.B) && this->C != other.C;
+        return eq(this->A*other.B, this->B*other.A) && this->C != other.C;
     }
 
     Line parallel(const Point& p) {
@@ -48,7 +48,7 @@ struct Line {
     }
 
     bool perpendicular(const Line& other) const {
-        return eq(this->A/other.B, this->B/other.A);
+        return eq(this->A*other.A, this->B*other.B);
     }
 
     Line perpendicular(const Point& p) {
